56f41d1c3ebc34e15e9d9bfbde5f57ef72b7a345
xffox <0xff0x666@gmail.com>
2013-01-07 17:32:12 +0000

    tests to produce xml output

--- util/test/test.cpp
+++ util/test/test.cpp
@@ -1,12 +1,37 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/TextOutputter.h>
+#include <cppunit/TextTestProgressListener.h>
+#include <fstream>
+#include <iostream>
+
+namespace
+{
+    const char *const OUTPUT_FILENAME = "util_test.xml";
+}
 
 int main()
 {
     CppUnit::TextUi::TestRunner runner;
+    CppUnit::TestResult controller;
+    CppUnit::TextTestProgressListener progressListener;
+    CppUnit::TestResultCollector result;
+    controller.addListener(&result);
+    controller.addListener(&progressListener);
+    std::ofstream xmlout(OUTPUT_FILENAME);
+    CppUnit::XmlOutputter xmlOutputter(&result, xmlout); 
+    CppUnit::TextOutputter consoleOutputter(&result, std::cout);
+
     CppUnit::TestFactoryRegistry &registry =
         CppUnit::TestFactoryRegistry::getRegistry();
     runner.addTest(registry.makeTest());
-    bool wasSuccessful = runner.run();
-    return !wasSuccessful;
+    runner.run(controller);
+
+    xmlOutputter.write();
+    consoleOutputter.write();
+
+    return !result.wasSuccessful();
 }
